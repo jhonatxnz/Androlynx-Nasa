813c5dcec959aefd7f473e0cc65e058a47adea49
Jhon <jw.jhonatan1711@gmail.com>
2022-10-02 23:57:29 -0300

    ...

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -12,7 +12,7 @@ export default function Menu(props) {
         <h2>Ever heard of the James Webb Telescope? It is currently the largest and most powerful in the world and due to its structure and power it needs to operate in space, in addition to having more than eighteen mirrors, in which it captures images of the cosmos. And to explain how this device works, we developed a game. In "Androlynx" the player poses as a scientist hired by NASA to operate it, with the aim of finding new galaxies, planets, constellations etc... And all its functioning and functions will be explained in the game, bringing the feeling of a scientist and more real space explorer, plus the possibility of finding out more about space and about James Webb.</h2>
 
         <div className='texto'>
-          <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY" title='bla'></iframe>
+        <a className="link" href="https://www.figma.com/file/7rQkXe62DQ33x7La8Dm4tD/Androlynx---game?node-id=0%3A1">Figma</a>
         </div>
 
       </div>
